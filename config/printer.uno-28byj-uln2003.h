2d2cdf1635f70b3d72cd5c25b794cfa1ddc9ce61
obigal <abuco1@msn.com>
2021-01-18 18:40:21 -0500

    Update printer.uno-28byj-uln2003.h

--- config/printer.uno-28byj-uln2003.h
+++ config/printer.uno-28byj-uln2003.h
@@ -35,7 +35,7 @@
 #define STEPS_PER_M_X            56000  // 56000 NOTE: X and Y must match
 #define STEPS_PER_M_Y            56000  // 56000
 #define STEPS_PER_M_Z            60000  // 60000
-#define STEPS_PER_M_E            58000  // 5800
+#define STEPS_PER_M_E            58000  // 58000
 
 /** \def MAXIMUM_FEEDRATE_X MAXIMUM_FEEDRATE_Y MAXIMUM_FEEDRATE_Z MAXIMUM_FEEDRATE_E
   Used for G0 rapid moves and as a cap for all other feedrates.
